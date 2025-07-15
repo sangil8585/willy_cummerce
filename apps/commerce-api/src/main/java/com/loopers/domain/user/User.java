43c082377a243298e44ab45e274f39eabe08c734
si.byun <sangil8585@naver.com>
2025-07-15 21:49:01 +0900

    유저 테스트 수정

--- apps/commerce-api/src/main/java/com/loopers/domain/user/User.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/user/User.java
@@ -1,5 +1,8 @@
 package com.loopers.domain.user;
 
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
+
 public record User(
         String id,
         String email,
@@ -8,12 +11,13 @@ public record User(
 ) {
     public User {
         if(id.length() > 10) {
-            throw new IllegalArgumentException("ID는 10자를 초과할 수 없습니다.");
+            throw new CoreException(ErrorType.BAD_REQUEST, "ID는 10자를 초과할 수 없습니다.");
         }
 
-        // 이메일 형식에 맞지 않으면 예외발생 (정상이멜이 예시 : sangil8585@naver.com)
         if(!email.matches("^[\\\\w.-]+@[\\\\w.-]+\\\\.[a-zA-Z]{2,}$")) {
-            throw new IllegalArgumentException("이메일 형식이 맞지 않습니다.");
+            throw new CoreException(ErrorType.BAD_REQUEST, "EMAIL 형식에 맞지 않습니다.");
         }
+
+
     }
 }
--- apps/commerce-api/src/test/java/com/loopers/domain/user/UserTest.java
+++ apps/commerce-api/src/test/java/com/loopers/domain/user/UserTest.java
@@ -1,10 +1,16 @@
 package com.loopers.domain.user;
 
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+
+@Slf4j
 public class UserTest {
 
     @Test
@@ -17,10 +23,13 @@ public class UserTest {
         Gender gender = Gender.valueOf("MALE");
 
         // When
-        assertThrows(IllegalArgumentException.class, () -> {
-            new User(id, email, birthday, gender);
-        });
+        CoreException ex = assertThrows(CoreException.class, () ->
+            new User(id, email, birthday, gender)
+        );
         // Then
+        assertEquals(ErrorType.BAD_REQUEST, ex.getErrorType());
+        assertEquals("ID는 10자를 초과할 수 없습니다.", ex.getCustomMessage());
+
     }
 
     @Test
@@ -32,8 +41,12 @@ public class UserTest {
         String birthday = "1993-02-24";
         Gender gender = Gender.valueOf("MALE");
 
-        // When & Then
-        assertThrows(IllegalArgumentException.class, () ->
-                new User(id, email, birthday, gender));
+        // When
+        CoreException ex = assertThrows(CoreException.class, () ->
+                new User(id, email, birthday, gender)
+        );
+        // Then
+        assertEquals(ErrorType.BAD_REQUEST, ex.getErrorType());
+        assertEquals("EMAIL 형식에 맞지 않습니다.", ex.getCustomMessage());
     }
 }
